c0195556f0cd2889e673858361dfe81cb1b17dc5
Joosep Tavits <joosep.tavits@cyber.ee>
2024-04-12 22:24:13 +0300

    format on commit

--- orchestrator/src/exception/validation_error.py
+++ orchestrator/src/exception/validation_error.py
@@ -1,23 +1,26 @@
 class FraudDetectionError(Exception):
     status_code = 400
-    
-    def __init__(self, message):            
+
+    def __init__(self, message):
         super().__init__(message)
+
 
 class TransactionVerificationError(Exception):
     status_code = 400
 
-    def __init__(self, message):            
+    def __init__(self, message):
         super().__init__(message)
+
 
 class SuggestionsError(Exception):
     status_code = 400
 
-    def __init__(self, message):            
-        super().__init__(message)       
+    def __init__(self, message):
+        super().__init__(message)
+
 
 class CheckoutError(Exception):
     status_code = 400
 
-    def __init__(self, message):            
+    def __init__(self, message):
         super().__init__(message)
